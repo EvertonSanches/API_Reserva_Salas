e7500786e24059b1410adae49f05294f0c233f25
Everton Sanches <74841165+EvertonSanches@users.noreply.github.com>
2026-04-13 23:08:55 +0000

    reserva 0.0.0.1 domain

--- domain/reserva.py
+++ domain/reserva.py
@@ -6,6 +6,7 @@ class Reserva:
         sala_id: int,
         data: str,
         hora_inicio: str,
+        hora_atual: str,
         hora_fim: str,
         status: str = "active"
     ):
@@ -14,17 +15,25 @@ class Reserva:
         self.sala_id = sala_id
         self.data = data
         self.hora_inicio = hora_inicio
+        self.hora_atual = hora_atual
         self.hora_fim = hora_fim
         self.status = status
 
-    def cancelar(self):
-        pass
+    def cancelar(self, usuario_id, data, hora_fim, status)-> bool:
+        if self.sala_id:
+                status == "active"
+                False
+                raise ValueError("Essa reserva já está cancelada")
+        return "Reserva Cancelada com sucesso!"
+        
 
-    def finalizar(self, hora_atual: str):
-        pass
+    def finalizar(self, hora_atual: str, hora_fim: str):
+        if hora_atual >= hora_fim:
+             return "Reserva Finalizada"
+             
 
-    def duracao_em_horas(self) -> float:
-        pass
+    def duracao_em_horas(self, hora_inicio: str, hora_fim: str) -> float:
+        
 
     def conflita_com(self, outra_reserva) -> bool:
         pass
